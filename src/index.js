b029c520b7196b25c7305a2926934c3146b36d77
vik1234-del <victor.okolo@pau.edu.ng>
2024-01-15 12:19:12 +0200

    latest

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <div>
-      {/* <BrowserRouter> */}
+      <BrowserRouter>
       <App />
-      {/* </BrowserRouter> */}
+      </BrowserRouter>
     </div>
-  </React.StrictMode>
+  {/* </React.StrictMode> */}
 );
 
 // If you want to start measuring performance in your app, pass a function
